5904dc3836a3080a3233fae1ac7b8392fc0f0939
Jtk764 <jknight911@utexas.edu>
2018-02-14 17:55:23 -0600

    Update ThreadSynch.java

--- HW2/src/ThreadSynch.java
+++ HW2/src/ThreadSynch.java
@@ -3,35 +3,40 @@
  * 
  */
 import java.util.concurrent.Semaphore; // for implementation using Semaphores
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ThreadSynch {
 	final Semaphore latch;
 	int threads;
 	final Semaphore mutex;
 	final Semaphore cond;
+	static AtomicInteger index;
 	
 	public ThreadSynch(int parties) {
 		 latch= new Semaphore(0);
 		threads=parties;
 		mutex=new Semaphore(1);
 		cond=new Semaphore(0);
+		index=new AtomicInteger();
 	}
 	
 	public int await() throws InterruptedException {
 		mutex.acquire();
-		int index;
-           index=latch.getQueueLength();
-           if (index== threads-1){
+		int index2;
+           index2=index.getAndIncrement();
+           if (index.get()== threads){
         	   latch.release(threads-1);
         	   cond.acquire();
+        	   index.getAndDecrement();
+        	   int tmp=latch.availablePermits();
                mutex.release();
            }
            else {
                mutex.release();
         	   latch.acquireUninterruptibly();
-        	   if (!latch.hasQueuedThreads()) {
+        	   if (index.getAndDecrement()==2) {
         		   cond.release();}
            }
-	    return index;
+	    return index2;
 	}
 }
